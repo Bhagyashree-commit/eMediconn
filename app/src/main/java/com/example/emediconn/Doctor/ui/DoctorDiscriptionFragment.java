6eacdf9c26613a9b9c473d51c81af6af8a4c9196
bhagyashree chaple <bhagyashreechaple@gmail.com>
2020-11-10 12:15:37 +0530

    change9

--- app/src/main/java/com/example/emediconn/Doctor/ui/DoctorDiscriptionFragment.java
+++ app/src/main/java/com/example/emediconn/Doctor/ui/DoctorDiscriptionFragment.java
@@ -13,6 +13,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.RatingBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -23,6 +24,7 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
+import com.bumptech.glide.Glide;
 import com.example.emediconn.Database.AppConfig;
 import com.example.emediconn.Database.PrefManager;
 import com.example.emediconn.Extras.Utils;
@@ -38,11 +40,21 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
+import de.hdodenhof.circleimageview.CircleImageView;
+
 public class DoctorDiscriptionFragment extends Fragment {
 ImageView ivpropic;
 ProgressDialog ploader;
 PrefManager prefManager;
-TextView tv_docname,tv_specialization,tv_education,tv_experiance,tv_fees,tv_avaibility;
+CircleImageView propic;
+RatingBar patientreview;
+    public static  String date_time;
+TextView tv_docname,tv_specialization,tv_reviewpostdate,tv_education,tv_servicesofdoctor,tv_experiance,tv_fees,tv_avaibility,tv_hospname,tv_destination,tv_PRpatientname,tv_PRheading,tv_PRstory;
+    private static final int SECOND_MILLIS = 1000;
+    private static final int MINUTE_MILLIS = 60 * SECOND_MILLIS;
+    private static final int HOUR_MILLIS = 60 * MINUTE_MILLIS;
+    private static final int DAY_MILLIS = 24 * HOUR_MILLIS;
+    ArrayList<String> mArraylist=new ArrayList<>();
 
     public DoctorDiscriptionFragment() {
         // Required empty public constructor
@@ -58,11 +70,22 @@ TextView tv_docname,tv_specialization,tv_education,tv_experiance,tv_fees,tv_avai
         // Inflate the layout for this fragment
         View v= inflater.inflate(R.layout.fragment_doctor_discription, container, false);
 tv_docname=v.findViewById(R.id.doctornameondescrip);
+propic=v.findViewById(R.id.doctorpropicondescrip);
 tv_specialization=v.findViewById(R.id.doctorspecializationondescrip);
 tv_education=v.findViewById(R.id.doctordeegreeondescrip);
 tv_experiance=v.findViewById(R.id.docexperianceondescrip);
 tv_fees=v.findViewById(R.id.feesondescription);
 tv_avaibility=v.findViewById(R.id.clinictimeondescrip);
+tv_hospname=v.findViewById(R.id.hospitalnameondescrip);
+tv_destination=v.findViewById(R.id.hospitaladdressondescrip);
+tv_PRpatientname=v.findViewById(R.id.PRpatientname);
+tv_PRheading=v.findViewById(R.id.PRheading);
+tv_PRstory=v.findViewById(R.id.PRstory);
+patientreview=v.findViewById(R.id.patientreview);
+tv_reviewpostdate=v.findViewById(R.id.reviewpostdate);
+//
+tv_servicesofdoctor=v.findViewById(R.id.doctorservice);
+
         //Back
         v.setFocusableInTouchMode(true);
         v.requestFocus();
@@ -85,7 +108,12 @@ tv_avaibility=v.findViewById(R.id.clinictimeondescrip);
         prefManager=new PrefManager(getActivity());
 
         if (Utils.isNetworkConnectedMainThred(getActivity())) {
-            getDoctorDiscription("9960664554");
+           // prefManager = new PrefManager(getContext());
+
+            HashMap<String, String> user = prefManager.getUserDetails();
+
+            String mobilenum = "9823650123";
+            getDoctorDiscription(mobilenum);
         } else {
             Toast.makeText(getActivity(), "No Internet Connection!", Toast.LENGTH_SHORT).show();
         }
@@ -110,7 +138,7 @@ tv_avaibility=v.findViewById(R.id.clinictimeondescrip);
 
         JSONObject obj = new JSONObject();
         try {
-            obj.put("mobilenumber", mobilenumber);
+            obj.put("mobilenumber", "9960664554");
 
 
         } catch (JSONException e) {
@@ -118,22 +146,80 @@ tv_avaibility=v.findViewById(R.id.clinictimeondescrip);
         }
 
         RequestQueue queue = Volley.newRequestQueue(getActivity());
-        JsonObjectRequest jsObjRequest = new JsonObjectRequest(Request.Method.POST, AppConfig.URL_GETDOCTORDISCRIPTION,obj,
+        final JsonObjectRequest jsObjRequest = new JsonObjectRequest(Request.Method.POST, AppConfig.URL_GETDOCTORDISCRIPTION,obj,
                 new Response.Listener<JSONObject>() {
                     @Override
                     public void onResponse(JSONObject response) {
                         ploader.dismiss();
-                        Log.d("DoctorResponse121",""+response);
+                        Log.d("Descriptionasasasasasas",""+response);
                         try {
+
                             if(response.getString("status").equalsIgnoreCase("true"))
                             {
-
-                                JSONArray jsonArray=response.getJSONArray("DoctorDiscription");
-                                for(int i=0;i<jsonArray.length();i++)
+                                JSONArray jsonArray = response .getJSONArray("DoctorDiscription");
+                                for(int i=0;i<jsonArray .length();i++)
                                 {
+                                    JSONObject jsonObject=jsonArray.getJSONObject(i);
 
+                                    String docname= jsonObject.getString("doctor_name");
+                                    String experience= jsonObject.getString("experience");
+                                    String fees= jsonObject.getString("fees");
+                                    String hospitalname= jsonObject.getString("hospitalname");
+                                    String hospitalcity= jsonObject.getString("hospitalcity");
+                                    String degree= jsonObject.getString("degree");
+                                    String proimage="http://healthcare.blucorsys.in/daccount/"+jsonObject.getString("profile_photo");
+
+                                    tv_docname.setText(docname);
+                                    tv_education.setText(degree);
+                                    tv_hospname.setText(hospitalname);
+                                    tv_destination.setText(hospitalcity);
+                                    tv_fees.setText(fees);
+                                    tv_experiance.setText(experience);
+                                    Glide.with(getActivity())
+                                            .load(proimage)
+                                            .into(propic);
+
+                                    JSONArray jsonAr = jsonObject .getJSONArray("patient_stories");
+                                    for(int j=0;j<jsonAr .length();j++)
+                                    {
+                                        jsonObject = jsonAr.getJSONObject(j);
+                                        String patient_name= jsonObject.getString("patient_name");
+                                        String story_heading= jsonObject.getString("story_heading");
+                                        String story= jsonObject.getString("story");
+                                        String feedBack= jsonObject.getString("feedBack");
+                                        String posted_date= jsonObject.getString("posted_date");
+                                        tv_PRpatientname.setText(patient_name);
+                                        tv_PRheading.setText(story_heading);
+                                        tv_PRstory.setText(story);
+                                        patientreview.setRating(Float.parseFloat(feedBack));
+                                        tv_reviewpostdate.setText(getTimeAgo(Long.parseLong(posted_date)));
+                                        Log.e("","Pname"+patient_name);
+                                    }
+
+                                    JSONArray jsonA = jsonObject .getJSONArray("specialization");
+                                    for(int j=0;j<jsonA .length();j++)
+                                    {
+                                        jsonObject = jsonA.getJSONObject(j);
+                                        String speciality= jsonObject.getString("speciality");
+                                        tv_specialization.setText(speciality);
+                                    }
+                                    JSONArray jsoA = jsonObject .getJSONArray("service");
+                                    for(int j=0;j<jsoA .length();j++) {
+                                        jsonObject = jsoA.getJSONObject(j);
+                                        String service = jsonObject.getString("service");
+                                        mArraylist.add(service);
+                                        //tv_servicesofdoctor.setText(service);
+                                    }
+                                        StringBuilder builder = new StringBuilder();
+                                        int index=0;
+                                        for (String details : mArraylist) {
+                                            index++;
+                                            builder.append(index +") "+details + "\n");
+                                        }
+                                        tv_servicesofdoctor.setText(builder.toString());
 
                                 }
+
                             }
 
                         } catch (JSONException e) {
@@ -163,4 +249,44 @@ tv_avaibility=v.findViewById(R.id.clinictimeondescrip);
         queue.add(jsObjRequest);
 
     }
+
+    public static String getTimeAgo(long time) {
+        if (time < 1000000000000L) {
+            // if timestamp given in seconds, convert to millis
+            time *= 1000;
+
+
+        }
+        // long now = getCurrentTime(ctx);
+        long now = System.currentTimeMillis();
+
+        if (time > now || time <= 0) {
+            return null;
+        }
+        // TODO: localize
+        final long diff = now - time;
+
+        if (diff < MINUTE_MILLIS) {
+            return "just now";
+        } else if (diff < 2 * MINUTE_MILLIS)
+        {
+            return "a minute ago";
+        } else if (diff < 50 * MINUTE_MILLIS) {
+            //mins = diff / MINUTE_MILLIS ;
+            return diff / MINUTE_MILLIS + " minutes ago";
+        } else if (diff < 90 * MINUTE_MILLIS) {
+            return "an hour ago";
+        } else if (diff < 24 * HOUR_MILLIS) {
+            if((diff/HOUR_MILLIS)==1)
+            {
+                return  "an hour ago";
+            }
+            else {
+                return diff / HOUR_MILLIS + " hours ago";
+            }
+        } else if (diff < 48 * HOUR_MILLIS) {
+            return "yesterday";
+        } else {
+            return date_time;
+        }    }
 }
